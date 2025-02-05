fd88212b2cc791fcdbd7326714ea52b6b70b8a79
Arifin Mahmud <arifin.cuet15@gmail.com>
2025-02-05 14:09:10 +0600

    Fixed Browser Setup Issue

--- src/test/java/TestAutomationJUnit.java
+++ src/test/java/TestAutomationJUnit.java
@@ -1,3 +1,4 @@
+import org.junit.Before;
 import org.junit.Test;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
@@ -6,16 +7,16 @@ import org.openqa.selenium.chrome.ChromeOptions;
 import java.time.Duration;
 
 public class TestAutomationJUnit {
+
     public WebDriver driver;
+
     @Before
     public void browserSetup(){
         ChromeOptions ops = new ChromeOptions();
-        ops.addArguments("--remote-allow-origins");
+        ops.addArguments("--remote-allow-origins=*");
         driver = new ChromeDriver(ops);
         driver.manage().window().maximize();
         driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
-
-
     }
 
     @Test
